808d00dc107d4b298335e7165c2fdf6089233d24
Mateusz <zubermateusz@gmail.com>
2023-03-23 18:26:39 +0100

    Days of the week

--- Topics/Throwing exceptions/Days of the week/src/Main.java
+++ Topics/Throwing exceptions/Days of the week/src/Main.java
@@ -2,8 +2,18 @@ import java.util.*;
 
 public class Main {
 
-    public static String getDayOfWeekName(int number) {
+    public static String getDayOfWeekName(int number) throws IllegalArgumentException{
         // write your code here
+        return switch (number) {
+            case 1 -> "Mon";
+            case 2 -> "Tue";
+            case 3 -> "Wed";
+            case 4 -> "Thu";
+            case 5 -> "Fri";
+            case 6 -> "Sat";
+            case 7 -> "Sun";
+                default -> throw new IllegalArgumentException();
+        };
     }
 
     /* Do not change code below */
