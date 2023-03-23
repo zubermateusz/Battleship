85dbe353a72be5ae9184810d2ec38249eb1b5ebb
Mateusz <zubermateusz@gmail.com>
2023-03-23 18:20:36 +0100

    initial commit

---
+++ Topics/Throwing exceptions/Days of the week/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+    public static String getDayOfWeekName(int number) {
+        // write your code here
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dayNumber = scanner.nextInt();
+        try {
+            System.out.println(getDayOfWeekName(dayNumber));
+        } catch (Exception e) {
+            System.out.println(e.getClass().getName());
+        }
+    }
+}
