cb8400b6a1cc8ff40bf353c78c7db5d5a8eb3239
Mateusz <zubermateusz@gmail.com>
2023-03-27 17:38:14 +0200

    add class Player with private gameField, public method startGameField

---
+++ Battleship/task/src/battleship/Player.java
@@ -0,0 +1,15 @@
+package battleship;
+
+
+public class Player {
+
+    private char[][] gameField = new char[22][11];
+
+    private void startGameField(){
+        // add first row
+        char[] tab = new char[]{' ', ' ', '1', ' ', '2', ' ', '3', ' ', '4', ' ', '5', ' ', '6', ' ', '7', ' ', '8', ' ', '9', ' ', '1','0'};
+
+    }
+
+
+}
