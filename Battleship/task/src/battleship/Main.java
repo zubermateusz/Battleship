1777df93bd427f1e18ddcbac41036698c723c44e
Mateusz <zubermateusz@gmail.com>
2023-03-27 17:45:15 +0200

    add showGameFiled method

--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         // Write your code here
+        Player player1 = new Player();
     }
 }
--- Battleship/task/src/battleship/Player.java
+++ Battleship/task/src/battleship/Player.java
@@ -5,11 +5,21 @@ public class Player {
 
     private char[][] gameField = new char[22][11];
 
+
+    public Player(){
+        startGameField();
+        showGameField();
+    }
+
     private void startGameField(){
         // add first row
-        char[] tab = new char[]{' ', ' ', '1', ' ', '2', ' ', '3', ' ', '4', ' ', '5', ' ', '6', ' ', '7', ' ', '8', ' ', '9', ' ', '1','0'};
+        gameField[0] = new char[]{' ', ' ', '1', ' ', '2', ' ', '3', ' ', '4', ' ', '5', ' ', '6', ' ', '7', ' ', '8', ' ', '9', ' ', '1','0'};
 
     }
 
-
+    public void showGameField(){
+        for (char[] chars : this.gameField) {
+            System.out.print(chars);
+        }
+    }
 }
