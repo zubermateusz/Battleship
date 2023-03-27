81a06aa65f52753280d48b2f0d06e1e03ea16042
Mateusz <zubermateusz@gmail.com>
2023-03-27 18:13:04 +0200

    add flag isAlive to Ships.java

--- Battleship/task/src/battleship/Ships.java
+++ Battleship/task/src/battleship/Ships.java
@@ -2,26 +2,32 @@ package battleship;
 
 public enum Ships {
 
-    AIRCRAFT_CARRIER(5, false),// 5- size of ship, false - is not set on field game
-    BATTLESHIP(4, false),
-    SUBMARINE(3, false),
-    CRUISER(3, false),
-    DESTROYER(2, false);
+    AIRCRAFT_CARRIER(5, false, true),// 5- size of ship, false - is not set on field game
+    BATTLESHIP(4, false, true),
+    SUBMARINE(3, false, true),
+    CRUISER(3, false, true),
+    DESTROYER(2, false, true);
 
-    private boolean flag;
-    Ships(int i, boolean setFalg) {
-        this.flag = setFalg;
+    private boolean isSet;
+    private boolean isAlive;
+    Ships(int i, boolean setFalg, boolean isAlive) {
+        this.isSet = setFalg;
+        this.isAlive = isAlive;
     }
 
-    public void setFlag(boolean flag){
-        this.flag = flag;
+    public void setSet(boolean set){
+        this.isSet = set;
     }
 
-    public boolean getFlag(){
-        return this.flag;
+    public boolean getSet(){
+        return this.isSet;
     }
 
+    public boolean isAlive() {
+        return isAlive;
+    }
 
-
-
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 }
