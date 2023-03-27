ad197ea0d104ddbd2f325cbf2d1b747240f24eeb
Mateusz <zubermateusz@gmail.com>
2023-03-27 19:18:00 +0200

    add coordinatesAfterChange to change F3 F7 to coordinates for field (works good)

--- Battleship/task/src/battleship/Player.java
+++ Battleship/task/src/battleship/Player.java
@@ -1,8 +1,11 @@
 package battleship;
 
 
+import java.awt.geom.Point2D;
+
 public class Player {
 
+    //The symbol O denotes a cell with your ship, X denotes that the ship was hit, and M signifies a miss.
     private char[][] gameField = new char[11][22];
 
 
@@ -26,6 +29,24 @@ public class Player {
         }
     }
 
+    public void setShipOnGameField(Ships ship, String playerCoordinates){
+        //split playerCoordinates to two coordinates,
+        //after split first is row A B C .. and columnt 0 - 10
+        System.out.println(coordinatesAfterChange(playerCoordinates)[0][0]);
+        System.out.println(coordinatesAfterChange(playerCoordinates)[0][1]);
+        System.out.println(coordinatesAfterChange(playerCoordinates)[1][0]);
+        System.out.println(coordinatesAfterChange(playerCoordinates)[1][1]);
+    }
+
+    private int[][] coordinatesAfterChange(String coordinates){
+        int[][] coordinatesForField = new int[2][2];
+        String[] coordinatesAfterSplit = coordinates.split(" ");
+        coordinatesForField[0][0] = coordinatesAfterSplit[0].charAt(0) - 65;// row A B C... (- 65 kod ASCII 'A'
+        coordinatesForField[0][1] = Integer.parseInt(coordinatesAfterSplit[0].substring(1));// column 1 2 3 ...
+        coordinatesForField[1][0] = coordinatesAfterSplit[1].charAt(0) - 65;// row A B C...
+        coordinatesForField[1][1] = Integer.parseInt(coordinatesAfterSplit[1].substring(1));// column 1 2 3 ...
+        return coordinatesForField;
+    }
     public void showGameField(){
         for (char[] chars : this.gameField) {
             System.out.print(chars);
