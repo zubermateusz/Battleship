706ba689d75b1674c10e684a85f6c3fa6d27cb8d
Mateusz <zubermateusz@gmail.com>
2023-03-28 18:23:04 +0200

    add String name to Enum Ships.java

--- Battleship/task/src/battleship/Ships.java
+++ Battleship/task/src/battleship/Ships.java
@@ -2,19 +2,25 @@ package battleship;
 
 public enum Ships {
 
-    AIRCRAFT_CARRIER(5, false, true),// 5- size of ship, false - is not set on field game
-    BATTLESHIP(4, false, true),
-    SUBMARINE(3, false, true),
-    CRUISER(3, false, true),
-    DESTROYER(2, false, true);
+    AIRCRAFT_CARRIER("Aircraft Carrier" , 5, false, true),// 5- size of ship, false - is not set on field game
+    BATTLESHIP("Battleship", 4, false, true),
+    SUBMARINE("Submarine", 3, false, true),
+    CRUISER("Cruiser", 3, false, true),
+    DESTROYER("Destroyer", 2, false, true);
 
     private boolean isSetOfField;
     private boolean isAlive;
     int length;
-    Ships(int length, boolean setFalg, boolean isAlive) {
+    String name;
+    Ships(String name, int length, boolean setFalg, boolean isAlive) {
+        this.name = name;
         this.isSetOfField = setFalg;
         this.isAlive = isAlive;
         this.length = length;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getLength() {
